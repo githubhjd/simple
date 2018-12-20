0780a9d0f17fc9764904e81d50415a21b83a6525
githubhjd <2635831652@qq.com>
2018-12-20 17:22:19 +0800

    test1

--- src/main/java/com/speingboot/simple/controller/TestController.java
+++ src/main/java/com/speingboot/simple/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.speingboot.simple.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,9 +32,9 @@ public class TestController {
         return modelAndView;
     }
 
-//    @RequestMapping("/api")
-//    public ModelAndView api(){
-//        ModelAndView modelAndView = new ModelAndView("api");
-//        return modelAndView;
-//    }
+    @RequestMapping("/api")
+    public ModelAndView api(){
+        ModelAndView modelAndView = new ModelAndView("api");
+        return modelAndView;
+    }
 }
